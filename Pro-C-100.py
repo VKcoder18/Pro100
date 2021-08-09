df3137e8c0bbf4070ce2bfcfbf22dffa88db627c
VKcoder18 <67476434+VKcoder18@users.noreply.github.com>
2021-08-09 18:43:59 -0500

    Add files via upload

---
+++ Pro-C-100.py
@@ -0,0 +1,10 @@
+class Atm:
+    def __init__(self,pin, amount):
+        self.pin=pin
+        self.amount=amount
+
+    def start(CashWithdrawl):
+        print('CashWithdrawl')
+
+    def stop(BalanceEnquiry):
+        print('BalanceEnquiry')
